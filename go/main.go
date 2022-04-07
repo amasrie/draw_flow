52729a50f9fef634f9accc57ed5942de6f6467c2
abdalah.masrie <abdalah.masrie@sigis.com.ve>
2022-04-07 11:01:52 -0400

    adding chi to go project

--- go/main.go
+++ go/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+    "net/http"
+    "log"
+    "github.com/go-chi/chi/v5"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+    port := "9020"
+    log.Printf("Starting up on http://localhost:%s", port)
+    route := chi.NewRouter()
+
+    route.Get("/", func(writer http.ResponseWriter, request *http.Request) {
+      writer.Header().Set("Content-Type", "text/plain")
+      writer.Write([]byte("Hello World!"))
+    })
+
+    log.Fatal(http.ListenAndServe(":" + port, route))
 }
